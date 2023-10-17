67b4c983d9e9ea6fc9117057edda5fe5e9cc0366
Salvation Amoke <salvationamoke@gmail.com>
2023-10-17 15:50:59 +0100

    modified:   src/Layouts/mainlayout.jsx 	deleted:    src/components/Socialtab.jsx

--- src/Layouts/mainlayout.jsx
+++ src/Layouts/mainlayout.jsx
@@ -1,12 +1,11 @@
 import { Link, Outlet } from "react-router-dom"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import Socialtab from "../components/socialtab"
 const mainlayout = () => {
     return (
         <>
             <Header />
-            <Socialtab />
+            <Link to='social' className="fixed top-3/4 -right-9 bg-darkpink px-8 py-1 rotate-90">social</Link>
             <main><Outlet /></main>
 
             <Footer />
