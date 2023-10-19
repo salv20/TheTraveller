44a606ab9fa2f4cf6020190d9880660b125dcb8f
Salvation Amoke <salvationamoke@gmail.com>
2023-10-19 07:39:25 +0100

    rename 	modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const [open, setOpen] = useState(true)
     return (
         <header className="flex justify-around py-6 font-bold bg-heading">
-            <h1 className="uppercase text-lg">travel</h1>
+            <h1 className="uppercase text-lg">Easytravel</h1>
             <nav className={` ${open ? '-left-eighty' : 'left-0'} transition-all duration-500 bg-dawhite top-0 w-3/5 h-full md:px-0 md:w-auto md:bg-transparent  fixed md:static md:flex gap-x-8 capitalize text-lightgray`}>
                 <button type="button" className="md:hidden text-3xl  relative left-3/4 py-5" onClick={() => setOpen(!open)}> <FaTimes /></button>
                 <ul className="grid gap-y-1 md:flex gap-x-8">
