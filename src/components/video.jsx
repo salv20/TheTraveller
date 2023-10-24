69f564093079e5b77c3b988459e112aa7821cd93
Salvation Amoke <salvationamoke@gmail.com>
2023-10-24 10:12:14 +0100

    modified:   src/components/video.jsx

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -3,14 +3,16 @@ import 'react-html5video/dist/styles.css'
 
 const VideoPage = () => {
     return (
-        <div className="mx-auto mt-10 w-3/4">
-            <Video autoPlay loop muted
-                controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
-                poster="http://sourceposter.jpg"
-            >
-                <source src="/VID.mp4" type="video/mp4" />
-            </Video>
-        </div>
+        <section className=" bg-heading absolute h-full w-full">
+            <div className="mx-auto pt-20 lg:pt-10 w-3/4 lg:w-3/5">
+                <Video autoPlay loop muted
+                    controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
+                    poster="http://sourceposter.jpg"
+                >
+                    <source src="/VID.mp4" type="video/mp4" />
+                </Video>
+            </div>
+        </section>
     )
 }
 
