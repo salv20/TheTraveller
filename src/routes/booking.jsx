8643f39aa79f2e16fa48ee06cb3895b20a97fac7
Salvation Amoke <salvationamoke@gmail.com>
2023-11-02 21:49:51 +0100

    modified:   src/routes/booking.jsx

--- src/routes/booking.jsx
+++ src/routes/booking.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import DatePicker from "react-datepicker";
-import { FaMapMarkerAlt, FaCalendarAlt, FaPlane, FaCircle, FaLandmark, FaLine, FaMarsStroke, FaMinus } from 'react-icons/fa'
-
+import { FaMapMarkerAlt, FaCalendarAlt, FaCircle } from 'react-icons/fa'
 import "react-datepicker/dist/react-datepicker.css";
+
+
 const Booking = () => {
     const [startDate, setStartDate] = useState(new Date());
     const [book, setBook] = useState(false)
     return (
-        <section className="space-y-6">
+        <section className="space-y-6 pb-8">
             <article className="pr-1 font-semibold text-lg">
                 <p>Online booking system for all services based industies and individals.
                     <span>We offer the easiest and the best booking services. Book your flight 24hrs with us.</span>
@@ -18,16 +19,17 @@ const Booking = () => {
                 onClick={() => setBook(!book)}
             >Book here</button>
             <article className={`${!book ? 'hidden' : 'block'}`}>
-                <form className='grid sm:grid-cols-2 gap-x-5 gap-y-5 capitalize font-semibold'>
+                <form
+                    className='grid sm:grid-cols-2 gap-x-5 gap-y-5 capitalize font-semibold'>
                     <div className="">
                         <FaMapMarkerAlt className=" inline-block" />
                         <label>from</label>
-                        <input type="text" className="block outline-none border-b-2 border-lightgray w-full" />
+                        <input type="text" required className="block outline-none border-b-2 border-lightgray w-full" />
                     </div>
 
                     <div>
                         <label>to</label>
-                        <input type="text" className="block outline-none border-b-2 border-lightgray w-full" />
+                        <input type="text" required className="block outline-none border-b-2 border-lightgray w-full" />
                     </div>
 
                     <div className="">
@@ -35,22 +37,22 @@ const Booking = () => {
                             <FaCalendarAlt className="mt-1" />
                             <label>depart</label>
                         </div>
-                        <p className="border-lightgray border-b-2">
+                        <div className="border-lightgray border-b-2">
                             {<DatePicker className=" outline-none" selected={startDate} onChange={(date) => setStartDate(date)} />}
-                        </p>
+                        </div>
                     </div>
 
                     <div className="">
                         <label className="pb-2 block">
                             return
                         </label>
-                        <p className="border-lightgray border-b-2">{<DatePicker className=" outline-none" selected={startDate} onChange={(date) => setStartDate(date)} />}</p>
+                        <div className="border-lightgray border-b-2">{<DatePicker className=" outline-none" selected={startDate} onChange={(date) => setStartDate(date)} />}</div>
                     </div>
                     <div className="">
                         <label className="block">
                             passenger
                         </label>
-                        <input type="number" className="block outline-none border-b-2 border-lightgray w-full" />
+                        <input type="number" required className="block outline-none border-b-2 border-lightgray w-full" />
                     </div>
                     <div>
                         <label className="block">
@@ -73,11 +75,13 @@ const Booking = () => {
                             <option value="british airline">british airline</option>
                         </select>
                     </div>
-                    <button type="submit" className=" mx-auto px-14 py-3 capitalize rounded-3xl bg-orange font-bold text-white">Book flight</button>
+                    <button type="submit"
+                        className=" mx-auto px-14 py-3 capitalize rounded-3xl bg-orange font-bold text-white"
+                    >Book flight</button>
                 </form>
             </article>
 
-            <article className=" capitalize font-semibold space-y-3 py-5 bg-heading rounded-r-full px-4">
+            <article className=" capitalize font-semibold space-y-3 bg-heading rounded-r-full p-4">
                 <h1 className="font-bold text-lg pb-3">all bookings</h1>
                 <div className="grid grid-cols-3 gap-x-2">
                     <p>
@@ -86,8 +90,7 @@ const Booking = () => {
                     </p>
                     <div className="">
                         <p>5hrs 15min</p>
-                        <p className="flex pt-1 relative right-3.5"> <FaCircle className=" text-orange relative text-sm bottom-1.5 " /><hr className="w-20 h-0.5 bg-orange" />  <FaCircle className="relative bottom-1.5 text-sm text-orange" /></p>
-
+                        <div className="flex pt-1 relative right-3.5"> <FaCircle className=" text-orange relative text-sm bottom-1.5 " /><hr className="w-20 h-0.5 bg-orange" />  <FaCircle className="relative bottom-1.5 text-sm text-orange" /></div>
                     </div>
                     <p>
                         09:55
