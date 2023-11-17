dbeccbed54f36570c1ac3dd2c34d76a046b56990
Salvation Amoke <salvationamoke@gmail.com>
2023-11-17 11:17:25 +0100

    modified:   src/components/hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,7 +5,7 @@ const Hero = ({ search, setSearch }) => {
     const navigate = useNavigate()
     const ref = useRef()
     return (
-        <form className="flex justify-between py-5 space-x-3">
+        <div className="flex justify-between py-5 space-x-3">
             <input
                 type="search" name="" id="" placeholder="Search destinations" className="outline-none py-2 rounded-lg text-center w-3/4 border-2"
                 value={search}
@@ -18,17 +18,15 @@ const Hero = ({ search, setSearch }) => {
                 }
             />
             <button
-                type="button" aria-label="search"
-
+                type="button"
+                aria-label="search"
                 className=" capitalize bg-orange text-white font-bold text-center px-6 rounded-lg"
-                onClick={(e) => {
-                    e.preventDefault()
+                onClick={() => {
                     ref.current.focus()
-                    search && setSearch(search)
-                    return false
+                    setSearch(search)
                 }}
             >search</button>
-        </form>
+        </div>
     )
 }
 
