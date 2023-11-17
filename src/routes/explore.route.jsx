d493564a606d4d64a07eaffd85b773355f104751
Salvation Amoke <salvationamoke@gmail.com>
2023-11-18 00:19:51 +0100

    serch functionality 	deleted:    src/Pages/explore/locations.jsx 	modified:   src/index.css 	modified:   src/routes/explore.route.jsx 	modified:   src/routes/favorite.jsx 	modified:   utilities/accesory.js

--- src/routes/explore.route.jsx
+++ src/routes/explore.route.jsx
@@ -1,42 +1,71 @@
-import { FaHeart, FaArrowRight } from 'react-icons/fa'
-import { top } from '../../utilities/accesory'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import Locations from '../Pages/explore/locations';
 
-
+import { useEffect, useState } from 'react'
+import { FaHeart } from 'react-icons/fa'
+import { CountryApi } from '../components/Api'
+import { useOutletContext } from 'react-router-dom'
 const Explore = () => {
+    const [countryData, setCountry] = useState([])
+    const flags = []
+    const names = []
+    const search = useOutletContext()
 
-    const image = top.img
-    const name = top.name
+    useEffect(() => {
+        const countries = async () => {
+            const data = await CountryApi()
+            const finalData = (data?.filter(country => ((country.name.common).toLowerCase()).includes(search.toLowerCase())))
+            setCountry(finalData)
+        }
+        countries()
+    }, [search])
+
+    const storage = () => {
+        localStorage.setItem('restflags', JSON.stringify(flags))
+        localStorage.setItem('restnames', JSON.stringify(names))
+        const restnames = JSON.parse(localStorage.getItem('restnames'))
+        const restflags = JSON.parse(localStorage.getItem('restflags'))
+        console.log(restflags, restnames);
+        return [restflags, restnames]
+    }
     return (
-        <section>
-            <h1 className='font-bold uppercase text-center'>dicover the world</h1>
+        <section className='grid grid-cols-2 gap-4'>
 
-            <Swiper
-                slidesPerView={2}
-                spaceBetween={10}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Pagination]}
-                className="pb-10 my-2"
-            >
-                {image.map((img, index) => (
-                    <SwiperSlide key={index} className='mb-12 shadow-lg bg-services p-1 bg-white'>
-                        <img src={img} alt="" className='w-full h-36 sm:h-52' />
-                        <h1 className='text-orange capitalize text-sm'>{name[index]}</h1>
-                        <div className='flex justify-between py-2 pr-2'>
-                            <button className='bg-visit py-1 px-5 rounded-lg font-bold'>Visit</button>
-                            <button type="button" aria-label='likebtn' className='hover:text-orange text-black transition-all duration-500 text-xl h-fit mt-1'><FaHeart /></button>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+            {countryData?.map((country, index) => (
+                <div key={index} className='shadow-lg bg-services p-1 bg-white '>
+                    <img src={country.flags.svg} alt="" className='w-full h-36 sm:h-52' />
+                    <p>{country.name.common}</p>
+                    <div className='flex justify-between py-2 pr-2'>
+                        <button className=' bg-visit py-1 px-5 rounded-lg font-bold'
+                            onClick={(e) => console.log(e.target)}
+                        >Visit</button>
+                        <button type="button"
+                            aria-label='likebtn'
+                            className='transition-all duration-500 text-xl h-fit mt-1  text-black'
+                        ><FaHeart
+                                onClick={e => {
+                                    const target = e.currentTarget
+                                    target.classList.toggle('liked')
+                                    const parentElement = (target.closest('div').parentElement.children)
+                                    const flag = parentElement[0].src
+                                    const name = parentElement[1].textContent
 
-            <Locations />
+                                    // ADDING AND REMOVING FLAG SRC AND COUNTRY NAME
+                                    if (target.classList.contains('liked')) {
+                                        flags.push(flag)
+                                        names.push(name)
+                                        storage()
+                                    } else {
+                                        const index = flags.findIndex((src) => src === flag)
+                                        flags.splice(index, 1)
+                                        names.splice(index, 1)
+                                        storage()
+                                    }
+
+                                }} />
+                        </button>
+                    </div>
+                </div>
+            ))}
+
         </section>
     )
 }
--- src/routes/favorite.jsx
+++ src/routes/favorite.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import { top } from '../../utilities/accesory';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 const Favorite = () => {
+
+    const image = top.img
+    const name = top.name
+
+
     return (
         <div>
-            <h1>favorite</h1>
+            <h2 className=' capitalize text-orange font-bold text-center pt-4'>favorites</h2>
+            <Swiper
+                slidesPerView={2}
+                spaceBetween={10}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="pb-10 my-2"
+            >
+                {image.map((img, index) => (
+                    <SwiperSlide key={index} className='mb-12 shadow-lg bg-services p-1 bg-white'>
+                        <img src={img} alt="" className='w-full h-36 sm:h-52' />
+                        <h1 className='text-orange capitalize text-sm text-center'>{name[index]}</h1>
+                        <div className=' grid px-5'>
+                            <button className='bg-visit py-1 px-5 rounded-lg font-bold'>Visit</button>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     )
 }
