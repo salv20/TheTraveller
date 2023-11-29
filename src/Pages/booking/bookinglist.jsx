dc87aa89ad4396ab6fef14bcd2730591a65b4c5f
Salvation Amoke <salvationamoke@gmail.com>
2023-11-29 11:43:24 +0100

    AXIOS FETCH AND DISPLAY ALL BOOKINGS 	modified:   package-lock.json 	modified:   package.json 	modified:   src/Pages/booking/bookinglist.jsx

--- src/Pages/booking/bookinglist.jsx
+++ src/Pages/booking/bookinglist.jsx
@@ -1,34 +1,51 @@
 import { FaCircle, FaTrashAlt } from 'react-icons/fa'
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 const Bookinglist = () => {
+    const [Details, setDetails] = useState({})
+
+    useEffect(() => {
+        const countries = async () => {
+            try {
+                const data = await axios.get('http://localhost:3010/Bookings')
+                setDetails(data.data)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        countries()
+    }, [])
 
     return (
-        <article className="capitalize font-semibold space-y-3 bg-heading rounded-r-full p-3">
+        <article className="capitalize font-semibold space-y-5">
+
             <h1 className="font-bold text-lg pb-3">all bookings</h1>
-
-            <div className="flex">
-                <div className="w-full space-y-3">
-                    <div className="grid grid-cols-3 gap-x-2">
-                        <p> nigeria </p>
-                        <div>
-                            <p>5hrs 15min</p>
-                            <div className="flex  relative right-1.5 sm:right-3.5"> <FaCircle className=" text-orange relative text-sm bottom-1.5 " /><hr className="w-20 h-0.5 bg-orange" />  <FaCircle className="relative bottom-1.5 text-sm text-orange" /></div>
+            {(Details.from)?.map((country, index) => (
+                <div className="flex bg-heading rounded-r-full p-3 py-8" key={index}>
+                    <div className="w-full space-y-3" >
+                        <div className="grid grid-cols-3 gap-x-2">
+                            <p> {country}</p>
+                            <div>
+                                <p>{Details.duration[index]}</p>
+                                <div className="flex  relative right-1.5 sm:right-3.5"> <FaCircle className=" text-orange relative text-sm bottom-1.5 " /><hr className="w-20 h-0.5 bg-orange" />  <FaCircle className="relative bottom-1.5 text-sm text-orange" /></div>
+                            </div>
+                            <p> {Details.to[index]}</p>
                         </div>
-                        <p> canada </p>
+                        <hr className=" border-dotted bg-lightgray " />
+                        <div className="grid grid-cols-3">
+                            <p>{Details.airline[index]} airlines</p>
+                            <p>${Details.fee[index]}</p>
+                            <button type="button" className="w-fit px-8 py-2 bg-orange rounded-3xl text-white capitalize font-bold relative right-5 sm:right-0">edit</button>
+                        </div>
                     </div>
-                    <hr className=" border-dotted bg-lightgray " />
-                    <div className="grid grid-cols-3">
-                        <p>america airlines</p>
-                        <p>$20.50</p>
-                        <button type="button" className="w-fit px-8 py-2 bg-orange rounded-3xl text-white capitalize font-bold relative right-5 sm:right-0">edit</button>
-                    </div>
-                </div>
 
-                {/*  */}
-                <div className="text-3xl text-orange h-fit mt-8">
-                    <button type="button"><FaTrashAlt /></button>
+                    {/*  */}
+                    <div className="text-3xl text-orange h-fit mt-8">
+                        <button type="button"><FaTrashAlt /></button>
+                    </div>
                 </div>
-            </div>
+            ))}
+
         </article>
     )
 }
