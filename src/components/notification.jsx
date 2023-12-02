061b09e0577b15956baef16cdc40300c7ce237cc
Salvation Amoke <salvationamoke@gmail.com>
2023-12-02 16:41:17 +0100

    modified:   src/components/notification.jsx

--- src/components/notification.jsx
+++ src/components/notification.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 
 const Notification = ({ notify }) => {
     return (
         <div
-            className={`${!notify ? 'hidden' : 'block'} absolute px-2 py-5 bg-heading font-semibold w-3/5 mt-10 right-5 text-center`}>
+            className={`${!notify ? 'hidden' : 'block'} absolute px-2 py-5 bg-heading font-semibold w-3/5 mt-10 right-5 text-center rounded-xl`}>
             <p>Thank you for choosing us.</p>
         </div>
     )
