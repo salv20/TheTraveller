99bc7f8499c13378bdb47e666ecbaf291d2205d7
Salvation Amoke <salvationamoke@gmail.com>
2023-12-12 19:36:04 +0100

    modified:   src/components/Leftbar.jsx

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -4,7 +4,7 @@ import { navlist } from "../../utilities/accesory"
 
 const Leftbar = ({ nav, navFunc }) => {
     return (
-        <nav className={` ${!nav ? '-left-eighty' : 'left-0'} fixed  h-full bg-dawhite px-10 space-y-5 capitalize font-bold py-5  lg:sticky overflow-y-scroll lg:h-screen transition-all duration-700`}>
+        <nav className={` ${!nav ? '-left-eighty' : 'left-0'} fixed w-full md:w-3/4  h-full bg-dawhite px-10 space-y-5 capitalize font-bold py-5  lg:sticky overflow-y-scroll lg:h-screen transition-all duration-700`}>
             <button type="button" className="relative left-ninty text-2xl rounded-full p-1.5 lg:hidden bg-orange" onClick={navFunc}><FaTimes /></button>
             <h1 className=" text-lg md:text-xl ">Easy<span className="text-orange">travel</span></h1>
             <ul className="grid gap-y-2 font-bold text-lightgray w-fit ">
